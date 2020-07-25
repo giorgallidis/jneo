edd6508333808445c68de83af2911ecb0acc86cf
geoxo <59040520+geoxo@users.noreply.github.com>
2020-07-25 13:17:04 -0700

    Update frontpage.js

--- src/components/frontpage.js
+++ src/components/frontpage.js
@@ -34,12 +34,12 @@ const Frontpage = () => (
                   </div>
                   <div className="sqs-block html-block sqs-block-html" data-block-type="2" id="block-yui_3_17_2_48_1424560397197_11109">
                      <div className="sqs-block-content">
-                        <pre><code><Emoji label="rocket" symbol="🚀" />I apply material engineering, data science, biology and design thinking to make things that have the potential to make life better for humans. Check it out some my current and upcoming projects over at Dedalos.org.</code></pre>
-                        <pre><code><Emoji label="backpack" symbol="🎨" />I also apply those same principles to make art that inspires humans to be better and do more. Check out some of Projects. </code></pre>
-                        <pre><code><Emoji label="moneybag" symbol="🔬" /> I like doing research on human performance, longevity and disease prevention. You can read short blurps about that on Twitter or in longer form in the Essays section. You can also sign up for my newsletter to stay up to date when I post something new.</code></pre>
-                        <pre><code><Emoji label="one hundred" symbol="📕" /> My research has led me to publish a book HealBook.club among other things. </code></pre>
-                        <pre><code><Emoji label="handwriting" symbol="🎗️" /> I support causes that support environmental, humanitarian and animal interests among other things. You can read more about it in Causes. </code></pre>
-                        <pre><code><Emoji label="statue of liberty" symbol="📥" /> Give me a shout if you're interested in working with me on a project.  </code></pre>
+                        <pre><code><Emoji label="rocket" symbol="🚀" />'I apply material engineering, data science, biology and design thinking to make things that have the potential to make life better for humans. Check out some my current and upcoming projects over at Dedalos.'</code></pre>
+                        <pre><code><Emoji label="backpack" symbol="🎨" />'I also apply those same principles to make art that inspires humans to be better and do more. Check out some in Projects.'</code></pre>
+                        <pre><code><Emoji label="moneybag" symbol="🔬" /> 'I like doing research on human performance, longevity and disease prevention. You can read short blurps about that on Twitter or in longer form in the Essays section. You can also sign up for my newsletter to stay up to date when I post something new.'</code></pre>
+                        <pre><code><Emoji label="one hundred" symbol="📕" /> 'My research has led me to publish a book HealBook.club among other things.' </code></pre>
+                        <pre><code><Emoji label="handwriting" symbol="🎗️" /> 'I support causes that support environmental, humanitarian and animal interests among other things. You can read more about it in Causes.' </code></pre>
+                        <pre><code><Emoji label="statue of liberty" symbol="📥" /> 'Give me a shout if you are interested in working with me on a project.'  </code></pre>
                      </div>
                   </div>
                </div>
